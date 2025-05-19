bf5bad6aab8d1d3aa67f1ec464cfc023bd3a73f2
oduncica <oduncica@gmail.com>
2025-05-19 20:22:46 +0200

    add buid file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,12 +17,13 @@ export default function About() {
         <div>
           <h2 className="text-3xl font-bold mb-4">À propos de moi</h2>
           <p className="text-gray-300 mb-6 leading-relaxed">
-            Étudiante en Master Systèmes d’information à Paris Dauphine, je suis
-            passionnée par les technologies IT et la transformation numérique.
+            Étudiante en Master Cultures et Métiers du web à l'Université
+            Gustave Eiffel, je suis passionnée par les technologies IT et la
+            transformation numérique.
           </p>
           <ul className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-2 text-gray-400 list-disc list-inside">
             <li>Power Apps, Power BI, Power Automate</li>
-            <li>React, Node.js, HTML/CSS, JavaScript</li>
+            <li>React, Node.js, Express, HTML/CSS, JavaScript</li>
             <li>Méthodologies agiles (Scrum, Kanban)</li>
             <li>Anglais (C1) / Français (C2)</li>
           </ul>
